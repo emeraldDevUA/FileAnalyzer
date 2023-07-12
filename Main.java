325b6f0241fdbc53263cfc1073babdbc3ad2882e
Yurii <128812340+emeraldDevUA@users.noreply.github.com>
2023-07-13 01:00:31 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,18 +7,17 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
-    String path = "";
-    static HashMap<String, Integer> fileSizeMap = new HashMap<>();
 
-    static String[] formats = {".java",".c",".cpp",".glsl",".py", ".js"};
-    static gfxVessel vessel = new gfxVessel();
+    private static final HashMap<String, Integer> fileSizeMap = new HashMap<>();
 
+    private static final String[] formats = {".java",".c",".cpp",".glsl",".py", ".js"};
+    private static final gfxVessel vessel = new gfxVessel();
+
+    private static final String[] formatsWithIcons = {"Java☕", "C","C++","GLSL","Python\uD83D\uDC0D","JS"};
     public static void main(String[] args)  {
         File folder=null;
         JFileChooser jFileChooser = new JFileChooser();
         try {
-
-
             jFileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
             jFileChooser.showOpenDialog(vessel);
             File f = jFileChooser.getSelectedFile();
@@ -60,25 +59,47 @@ public class Main {
     public static void printMap(){
         int total = 0;
         int files = 0;
-
+        int[] formatFiles = new int[formats.length];
         Iterator<String> iterator = (fileSizeMap.keySet()).iterator();
 
         int tmp;
         Iterator<Integer> iterator1 = fileSizeMap.values().iterator();
         StringBuilder sb = new StringBuilder();
         while (iterator.hasNext()){
+            String temp;
             files++;
 
-            String str = String.format("%4d\t |%s\n",(tmp = iterator1.next()),iterator.next());
+            String str = String.format("%4d\t |%s\n",(tmp = iterator1.next()),(temp =iterator.next()));
             sb.append(str);
             System.out.print(str);
 
             total +=tmp;
+            int i = 0;
+
+            for (int j = 0; j < formats.length; j++) {
+                if(temp.contains(formats[j])){
+                    formatFiles[j]+=tmp;
+                    break;
+                }
+            }
+
         }
 
         System.out.println("---------------------");
         System.out.println(files+ ((files!=1)?"\tFiles":" File")+ ",\t" + total +" Lines of code");
         sb.append(files).append((files != 1) ? " Files" : " File").append(",\t").append(total).append(" Lines of code");
+
+        sb.append('\n').append('\n');
+
+        for(int i = 0; i< formatFiles.length; i++){
+            if(formatFiles[i] == 0){continue;}
+            sb.append(formatsWithIcons[i]).append("\t").append(formatFiles[i]).append('\t');
+                    sb.append((float) ((int)((100f*(float) formatFiles[i]/(float) total)*100))/100f).append("%").append("\n");
+
+        }
+
+
+
 
         vessel.setText(sb.toString());
     }
@@ -99,7 +120,7 @@ public class Main {
             scrollPane.add(textField);
             add(scrollPane);
             setVisible(true);
-
+            tf.setEditable(false);
             setTitle("FileAnalyzer");
             setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
